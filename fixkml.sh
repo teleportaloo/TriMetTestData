5d0092f0fec8e745fc2991e283d1354e93f0bff3
Andrew Wallace <pdxbus@me.com>
2018-05-27 08:27:36 -0700

    Added curl to shell script - so now it can be run easily every week to update test data.

--- fixkml.sh
+++ fixkml.sh
@@ -5,4 +5,4 @@
 #
 #  Created by Andrew Wallace on 5/26/18.
 #
-sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' tm_routes.kml | uniq > tm_routes_v3.kml
+curl https://developer.trimet.org/gis/data/tm_routes.kml | sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
