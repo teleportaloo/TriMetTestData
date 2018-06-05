d5fc592bd3c4cab89f4b1b4bc2770c2854aefd8c
Andrew Wallace <pdxbus@me.com>
2018-06-05 08:46:06 -0700

    Local copy of tm_routes to check if it changed
    Signed-off-by: Andrew Wallace <pdxbus@me.com>

--- fixkml.sh
+++ fixkml.sh
@@ -5,4 +5,6 @@
 #
 #  Created by Andrew Wallace on 5/26/18.
 #
-curl https://developer.trimet.org/gis/data/tm_routes.kml | sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
+
+curl https://developer.trimet.org/gis/data/tm_routes.kml > tm_routes.kml
+cat tm_routes.kml | sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
