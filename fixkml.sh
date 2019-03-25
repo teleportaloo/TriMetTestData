db1aa06cdc66177f1f4b1da634f6a9cc3fa2d812
Andrew Wallace <pdxbus@me.com>
2019-03-25 16:12:48 -0700

    Updates from TriMet and tweak to script
    Signed-off-by: Andrew Wallace <pdxbus@me.com>

--- fixkml.sh
+++ fixkml.sh
@@ -8,3 +8,4 @@
 
 curl https://developer.trimet.org/gis/data/tm_routes.kml > tm_routes.kml
 cat tm_routes.kml | sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
+git status
