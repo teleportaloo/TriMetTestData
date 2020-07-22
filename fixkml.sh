f394ab46231fa696b3414b4a10f25d19ae54597d
Andrew Wallace <pdxbus@me.com>
2020-07-22 12:33:44 -0700

    Updated for new TriMet file
    Signed-off-by: Andrew Wallace <pdxbus@me.com>

--- fixkml.sh
+++ fixkml.sh
@@ -7,5 +7,5 @@
 #
 
 curl https://developer.trimet.org/gis/data/tm_routes.kml > tm_routes.kml
-cat tm_routes.kml | sed -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
+cat tm_routes.kml | sed -e 's/,0.0//g' | sed -e 's/ id=\"[^"]*"//g' | sed  -e 's/"\([^"]*\)"/\1/' -e ':a' -e 'N' -e '$!ba' -e' s/<LineString>[[:space:]]*<coordinates>/<LineString><coordinates>/g' -e 's/<\/coordinates>[[:space:]]*<\/LineString>/<\/coordinates><\/LineString>/g' | uniq > tm_routes_v3.kml
 git status
